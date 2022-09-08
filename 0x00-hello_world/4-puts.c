a6d45bc1929ea5917b7333f20b4895f4e7eb52f3
Eboweme Ayo <ebowemeayo@yahoo.com>
2022-09-08 12:45:51 -0700

    To print out a sentence

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *
+ * * print_program - Prints "Programming is like building"
+ *
+ * * Return: 0
+ *
+ * */
+
+int print_program()
+
+{
+       	puts("Programming is like building a multilingual puzzle\n");
+       	return 0;
+
+}
