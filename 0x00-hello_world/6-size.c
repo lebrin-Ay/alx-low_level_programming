6573f63770ad09aaa62ecc667f473847592e6d4a
Eboweme Ayo <ebowemeayo@yahoo.com>
2022-09-08 14:00:39 -0700

    Removed trailing whitespace

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int main(void)
 	char charType;
 	float floatType;
 
-	printf("Size of a char: %zu byte(s)\n", sizeof (charType));
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(intLongType));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(intLongLongType));
