9bf302d4593758acb0794ff3a06eb27cbd8d496b
Eboweme Ayo <ebowemeayo@yahoo.com>
2022-09-13 13:04:35 -0700

    Program that prints _putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef _MAIN_C_
+#define _MAIN_C_
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int);
+int print_last_digit(int);
+int print_sign(int n);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+
+#endif
