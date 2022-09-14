2d091780af6974e2bbead38158ea9ba5f36626e3
Eboweme Ayo <ebowemeayo@yahoo.com>
2022-09-14 08:00:25 -0700

    Removed header files

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,28 +5,16 @@
  */
 void print_to_98(int n)
 {
-	int i;
-
-	if (n > 98)
+	if (n >= 98)
 	{
-		for (i = n; i >= 98; i++)
-		{
-			printf("%d", i);
-			if (i != 98)
-			{
-				printf(",");
-			}
-		}
-	} else
-	{
-		for (i = n; i <= 98; i++)
-		{
-			printf("%d", i);
-			if (i != 98)
-			{
-				printf(",");
-			}
-		}
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
 	}
-	printf("\n");
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
 }
