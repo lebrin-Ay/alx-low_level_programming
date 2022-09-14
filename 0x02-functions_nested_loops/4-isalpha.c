d281b677881fa65a4555a4e6a3226d84076cc3b3
Eboweme Ayo <ebowemeayo@yahoo.com>
2022-09-14 03:12:51 -0700

    Modified Task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,11 +2,11 @@
 /**
  * _isalpha - check the code for alphabetic characters
  * @c: letter to be checked
- * Return: 0
+ * Return: 1 if true, 0 if false
  */
 int _isalpha(int c)
 {
-	if ((c >= 65 && c <= 90) || (c >= 977 && c <= 122))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
