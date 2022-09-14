19e739f8f19dfcab9ea19f216651d56e632889fb
Eboweme Ayo <ebowemeayo@yahoo.com>
2022-09-14 08:31:18 -0700

    Optinal Tasks

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int add = 2;
+	long int a = 1;
+	long int b = a + 1;
+	long int c = a + b;
+
+	printf("ld, %ld, ", a, b);
+	while (add < 50)
+	{
+		printf("%ld", c);
+		add++;
+		a = b;
+		b = c;
+		c = a + b;
+		if (add < 50)
+		{
+			printf(",");
+		}
+	}
+	printf("\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int add = 2;
+
+	float a = 1;
+	float b = a + 1;
+	float c = a + b;
+
+	print("%.0f, ", a);
+	printf("%.0f, ", b);
+	while (add < 98)
+	{
+		add++;
+		printf("%.0f", c);
+		a = b;
+		b = c;
+		c = a + b;
+		if (add < 98)
+		{
+			printf(",");
+		}
+	}
+	printf("\n");
+	return (0);
+}
