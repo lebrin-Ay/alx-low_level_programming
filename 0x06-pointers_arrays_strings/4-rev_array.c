2f10778a9a931092d606b2b9d75df72fb769b654
Eboweme Ayo <ebowemeayo@yahoo.com>
2022-09-21 12:53:19 -0700

    Added the function the main.h file

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,6 +12,6 @@ void reverse_array(int *a, int n)
 	{
 		tmp = a[n - 1 - index];
 		a[n - 1 - index] = a[index];
-		a[index] = tmpa;
+		a[index] = tmp;
 	}
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,6 +9,7 @@ char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
 int _atoi(char *s);
+void reverse_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
 #endif
