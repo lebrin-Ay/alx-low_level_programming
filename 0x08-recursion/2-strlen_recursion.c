02f767dd851579f6898835790ef83c92b349f80f
Eboweme Ayo <ebowemeayo@yahoo.com>
2022-09-28 09:06:40 -0700

    _strlen_recursion function

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - calculates the lenght of a string
+ * @s: string to be used
+ *
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	int l = 0;
+
+	if (*s != '\0')
+	{
+		l++;
+		l += _strlen_recursion(s + 1);
+	}
+	return(l);
+}
