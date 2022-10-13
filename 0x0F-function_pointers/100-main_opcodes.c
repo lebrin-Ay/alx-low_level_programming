95ff2cb4e8f50fc658b0c9a924c67aded33ced7d
Eboweme Ayo <ebowemeayo@yahoo.com>
2022-10-13 11:27:13 -0700

    modified the file

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,39 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <udis86.h>
-
 /**
-  * main - ...
-  * @argc: ...
-  * @argv: ...
-  *
-  * Return: ...
-  */
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
+ * Return: Always 0.
+ */
 int main(int argc, char *argv[])
 {
-	ud_t ud_obj;
-	int val = 0, i = 0;
+		char *opc = (char *) main;
 
-	if (argc == 2)
-	{
-		val = atoi(argv[1]);
+		int i, nbytes;
 
-		if (val < 0)
+		if (argc != 2)
+		{
+			printf("Error\n");
+			exit(1);
+		}
+		nbytes = atoi(argv[1]);
+		if (nbytes < 0)
 		{
 			printf("Error\n");
 			exit(2);
 		}
-
-		ud_unit(&ud_obj);
-		ud_set_input_buffer(&ud_obj, argv[1], val);
-		ud_set_mode(&ud_obj, 64);
-		ud_set_syntax(&ud_obj, UD_SYN_INTEL);
-
-		while (ud_disassemble(&ud_obj))
+		for (i = 0; i < nbytes; i++)
 		{
-			printf("\t%s\n", ud_insn_hex(&ud_obj));
+			printf("%02x", opc[i] & 0xFF);
+			if (i != nbytes - 1)
+				printf(" ");
 		}
-	}
-
-	return (0);
+		printf("\n");
+		return (0);
 }
